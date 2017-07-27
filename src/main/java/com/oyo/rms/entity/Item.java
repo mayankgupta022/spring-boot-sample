698e93e671c9bd457613b20a7db2de3c6961aacd
oyomayankgupta <gupta.mayank@oyorooms.com>
2017-07-28 02:12:36 +0530

    Entity, Lombok, Auto-DDL

---
+++ src/main/java/com/oyo/rms/entity/Item.java
@@ -0,0 +1,63 @@
+package com.oyo.rms.entity;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "items")
+@ToString
+public class Item {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	Integer id;
+
+	@NotNull
+	@Getter @Setter private String name;
+
+	@NotNull
+	@Getter @Setter private Status status = Status.ACTIVE;
+
+	@NotNull
+	@Getter @Setter private LocalDateTime createdAt;
+
+	@NotNull
+	@Getter @Setter private LocalDateTime updatedAt;
+
+	@PrePersist
+	protected void onCreate() {
+		this.updatedAt = this.createdAt = LocalDateTime.now();
+	}
+
+	@PreUpdate
+	protected void onUpdate() {
+		this.updatedAt = LocalDateTime.now();
+	}
+
+	public enum Status {
+		ACTIVE("ACTIVE"), INACTIVE("INACTIVE");
+
+		String name;
+
+		Status(String value) {
+			name = value;
+		}
+
+		String getName() {
+			return name;
+		}
+	}
+
+}
--- src/test/java/com/oyo/rms/RmsApplicationTests.java
+++ src/test/java/com/oyo/rms/RmsApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.rms;
+package com.oyo.rms;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
